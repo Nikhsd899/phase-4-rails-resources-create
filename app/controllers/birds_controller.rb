6c896ca95bac238299e4a4ddd9f5feeda37186fc
Nicholas <nikhsd899@outlook.com>
2022-02-05 18:44:22 -0800

    Completed Assignment

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,4 +1,10 @@
 class BirdsController < ApplicationController
+ 
+  #POST /birds
+  def create
+    bird = Bird.create(name: params[:name], species: params[:species])
+    render json: bird, status: :created
+  end
 
   # GET /birds
   def index
@@ -14,6 +20,7 @@ class BirdsController < ApplicationController
     else
       render json: { error: "Bird not found" }, status: :not_found
     end
+  
   end
-
+   
 end
